357f21669f0824d84b89c3d57a51ff274f070ba3
Samuel Almeida <samuel.sousa@farme.com.br>
2025-01-15 16:02:12 -0300

    initial challenge code

---
+++ internal/entity/user_entity/user_entity.go
@@ -0,0 +1,16 @@
+package user_entity
+
+import (
+	"context"
+	"github.com/samuelralmeida/pge-auction/internal/internal_error"
+)
+
+type User struct {
+	Id   string
+	Name string
+}
+
+type UserRepositoryInterface interface {
+	FindUserById(
+		ctx context.Context, userId string) (*User, *internal_error.InternalError)
+}
